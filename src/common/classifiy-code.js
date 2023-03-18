42553df01ed3410d3d26064666ed8e3a70fae606
MinseoLee <mmmm@ajou.ac.kr>
2023-03-18 17:51:17 +0900

    feat: connect with dummies

--- src/common/classifiy-code.js
+++ src/common/classifiy-code.js
@@ -1,65 +1,71 @@
-const ClASSIFIY_CODE = {
-    "ajou-univ": {
+const ClASSIFIY_CODE = [
+    {
         name: "ajou-univ",
         kr: "아주대학교",
         icon: "https://www.ajou.ac.kr/_res/ajou/kr/img/intro/img-symbol.png",
         url: "https://www.ajou.ac.kr/kr/ajou/notice.do",
     },
-    "sw-college": {
+    {
         name: "sw-college",
         kr: "소프트웨어융합대학",
         icon: "https://cdn-icons-png.flaticon.com/128/6062/6062633.png",
         url: "https://sw.ajou.ac.kr/sw/board/notice.do",
     },
-    "sw-major": {
+    {
         name: "sw-major",
         kr: "소프트웨어학과",
         icon: "https://cdn-icons-png.flaticon.com/512/6062/6062646.png",
         url: "http://software.ajou.ac.kr/bbs/board.php?tbl=notice",
     },
-    "cs-major": {
+    {
         name: "cs-major",
         kr: "사이버보안학과",
         icon: "https://cdn-icons-png.flaticon.com/128/6062/6062414.png",
         url: "https://security.ajou.ac.kr/security/board/board01.jsp",
     },
-    "dm-major": {
+    {
         name: "dm-major",
         kr: "디지털미디어학과",
         icon: "https://cdn-icons-png.flaticon.com/128/6062/6062419.png",
         url: "https://media.ajou.ac.kr/media/board/board01.jsp",
     },
-    "ajou-lib": {
+    {
         name: "ajou-lib",
         kr: "도서관",
         icon: "https://cdn-icons-png.flaticon.com/512/1164/1164682.png",
         url: "https://library.ajou.ac.kr/#/bbs/notice?offset=0&max=20",
     },
-    "ajou-dorm": {
+    {
         name: "ajou-dorm",
-        kr: "기숙사식당",
+        kr: "생활관",
         icon: "https://cdn-icons-png.flaticon.com/512/3751/3751960.png",
-        url: "https://www.ajou.ac.kr/kr/life/food.do?mode=view&articleNo=63&article.offset=0&articleLimit=10",
+        url: "https://library.ajou.ac.kr/#/bbs/notice?offset=0&max=20",
     },
-    "ajou-teacher": {
-        name: "ajou-teacher",
-        kr: "교직원식당",
-        icon: "https://cdn-icons-png.flaticon.com/512/3631/3631947.png",
-        url: "https://www.ajou.ac.kr/kr/life/food.do?mode=view&articleNo=221&article.offset=0&articleLimit=10",
-    },
-    "gg": {
+    // {
+    //     name: "ajou-dorm",
+    //     kr: "기숙사식당",
+    //     icon: "https://cdn-icons-png.flaticon.com/512/3751/3751960.png",
+    //     url: "https://www.ajou.ac.kr/kr/life/food.do?mode=view&articleNo=63&article.offset=0&articleLimit=10",
+    // },
+    // {
+    //     name: "ajou-teacher",
+    //     kr: "교직원식당",
+    //     icon: "https://cdn-icons-png.flaticon.com/512/3631/3631947.png",
+    //     url: "https://www.ajou.ac.kr/kr/life/food.do?mode=view&articleNo=221&article.offset=0&articleLimit=10",
+    // },
+    {
         name: "gg",
         kr: "경기도청",
         icon: "https://www.gg.go.kr/site/gg/common/img/img-header-logo_230111.png",
         url: "https://youth.gg.go.kr/gg/info/housing-welfare.do",
     },
-    "scholar": {
+    {
         name: "scholar",
         kr: "한국장학재단",
         icon: "",
         // FIXME:: 한국장학재단 ~24:00 점검중.
         url: "",
     }
-};
+];
 
 export default ClASSIFIY_CODE;
--- src/components/notice-box/NoticeBox.jsx
+++ src/components/notice-box/NoticeBox.jsx
@@ -5,17 +5,17 @@ const NoticeBox = ({ selectedTab, noticeList }) => {
     return (
         <section className={S["container"]}>
             <h1 className={S["container-title"]}>Notices</h1>
-            {/*{noticeList.map((d, i) => {*/}
-            {/*    return (*/}
-            {/*        <a href={d.url} key={`notice-element-${i}`}>*/}
-            {/*            <div className={S["element"]}>*/}
-            {/*                <img src={ClASSIFIY_CODE[d.source].icon} />*/}
-            {/*                <span> [{ClASSIFIY_CODE[d.source].kr}] </span>*/}
-            {/*                <span> {d.title}</span>*/}
-            {/*            </div>*/}
-            {/*        </a>*/}
-            {/*    )*/}
-            {/*})}*/}
+            {noticeList.map((d, i) => {
+                return (
+                    <a href={d.url} key={`notice-element-${i}`}>
+                        <div className={S["element"]}>
+                            <img className={S["icon"]} src={ClASSIFIY_CODE[d.source].icon} />
+                            <span> [{ClASSIFIY_CODE[d.source].kr}] </span>
+                            <span> {d.title}</span>
+                        </div>
+                    </a>
+                )
+            })}
         </section>
     )
 }
